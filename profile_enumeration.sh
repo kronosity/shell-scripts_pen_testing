59c3fe2030dcbef1ad2b5602ce5ff802533f6209
Kronosity <46357671+kronosity@users.noreply.github.com>
2019-02-06 15:02:58 +0200

    Create profile_enumeration.sh

---
+++ profile_enumeration.sh
@@ -0,0 +1,15 @@
+!/bin/sh
+PROFILE=1
+while [$PORFILE -le 100000] ; do
+  echo -3 "$PROFILE ****** \n" >\> mapped.txt
+  `curl -v - G \
+  -H 'Cookie: uid=; sessid =' \
+  -d 'menu= ' \
+  -d 'userID='\
+  -d 'profile =$PROFILE'\
+  -d 'r='\
+  --url https://doman.com\ mapped.txt`
+  echo=e "*********"\n\n" >\> mapped.txt
+  USERID=`expr $PROFILE +1`
+  done exit
+  
